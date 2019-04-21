217a535e0cf3b790d50769cf131b89e35337a244
Dmitriy <idmitriyschegolev@yandex.ru>
2019-04-21 17:22:48 +0300

    categories update

--- my-app/src/app/pages/catalog/catalog.component.ts
+++ my-app/src/app/pages/catalog/catalog.component.ts
@@ -15,28 +15,7 @@ import { Categories } from 'src/app/models/categories';
 })
 export class CatalogComponent implements OnInit {
   products: Product[] = [];
-  categories = {
-    chairs: {
-       categoryName: 'productList__Chairs',
-       title: 'Стулья и кресла'
-    },
-    beds: {
-       categoryName: 'productList__Beds',
-       title: 'Кровати и матрасы'
-    },
-    cabinets: {
-       categoryName: 'productList__Cabinets',
-       title: 'Шкафы и комоды'
-    },
-    kitchen: {
-       categoryName: 'productList__Kitchen',
-       title: 'Мебель для кухни'
-    },
-    home: {
-       categoryName: 'productList__Home',
-       title: 'Товары для дома'
-    }
-  }
+  categories = Categories;
   error: any;
   title: string;
 

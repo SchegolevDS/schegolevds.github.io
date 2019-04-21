5f959d0bf30dae3f1a728659e89d291f2e6fff54
Dmitriy <idmitriyschegolev@yandex.ru>
2019-04-21 15:57:30 +0300

    remove pipe

--- my-app/src/app/pages/product-card/product-card.module.ts
+++ my-app/src/app/pages/product-card/product-card.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProductCardRoutingModule } from './product-card-routing.module';
 import { ProductCardComponent } from './product-card.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { SearchPipe } from './search.pipe';
 import { ImgCollapseComponent } from './img-collapse/img-collapse.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductCardComponent,
     BreadcrumbsComponent,
-    SearchPipe,
     ImgCollapseComponent],
   imports: [
     CommonModule,

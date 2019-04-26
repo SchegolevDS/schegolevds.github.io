12d2594c1cbe36be569c722d07ebb01cb1e7d0b2
Dmitriy <idmitriyschegolev@yandex.ru>
2019-04-26 19:46:01 +0300

    fix

--- my-app/src/app/pages/catalog/product-card-min/product-card-min.component.ts
+++ my-app/src/app/pages/catalog/product-card-min/product-card-min.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 import { Product } from 'src/app/models/product';
-import { FilterService } from 'src/app/services/filter.service';
 
 @Component({
   selector: 'app-product-card-min',
@@ -13,8 +12,7 @@ export class ProductCardMinComponent implements OnInit {
   @Input () product:Product;
   inCart: boolean = true;
 
-  constructor(private _cartService:ShoppingCartService,
-              public filter: FilterService) { }
+  constructor(private _cartService:ShoppingCartService) { }
 
   public addCart(product: any) {
     this._cartService.add(product);

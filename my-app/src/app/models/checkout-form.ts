010a9e272d7f2a9f9503f6e01cc7d946ce85fb60
Dmitriy <idmitriyschegolev@yandex.ru>
2019-04-29 15:57:52 +0300

    add phone validator, and alert-danger block

--- my-app/src/app/models/checkout-form.ts
+++ my-app/src/app/models/checkout-form.ts
@@ -12,5 +12,9 @@ export const CheckoutForm = new FormGroup({
                       Validators.required,
                       Validators.pattern("[a-zA-Z_.,-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}")
                   ]),
-  "phone": new FormControl("", Validators.required)
+  "phone": new FormControl("", [Validators.required,
+                          Validators.pattern("8\ [0-9]{3}\ [0-9]{3}-[0-9]{2}-[0-9]{2}")
+                        ])
 })
+
+//"8\ \([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" 8 (___) ___-__-__
